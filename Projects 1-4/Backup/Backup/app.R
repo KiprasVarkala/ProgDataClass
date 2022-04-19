7b599b48017b3e025480bff3bcfd3e8e15a8f21d
KiprasVarkala <kvpsych@gmail.com>
2022-04-19 00:32:17 -0400

    Cosmetics

--- Projects 1-4/Backup/Backup/app.R
+++ Projects 1-4/Backup/Backup/app.R
@@ -15,10 +15,10 @@ ui <- dashboardPage(
   dashboardHeader(title = "Projects"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("1. Test", tabName = "proj1",  icon = icon("image")),
-      menuItem("2. Test", tabName = "proj2", icon = icon("map")),
-      menuItem("3. Test", tabName = "proj3", icon = icon("file-code")),
-      menuItem("4. Test", tabName = "proj4", icon = icon("chart-bar"))
+      menuItem("1. Shiny App", tabName = "proj1",  icon = icon("file-code")),
+      menuItem("2. File Upload", tabName = "proj2", icon = icon("file-upload")),
+      menuItem("3. Visualization", tabName = "proj3", icon = icon("image")),
+      menuItem("4. Data Analysis Report", tabName = "proj4", icon = icon("chart-bar"))
     )
   ),
   
@@ -43,16 +43,43 @@ ui <- dashboardPage(
     #header code credit: https://stackoverflow.com/questions/45176030/add-text-on-right-of-shinydashboard-header
     tabItems(
       tabItem(tabName = "proj1",
-              h2("Project 1")
+              h2("How to Compile your Work into One Simple App Using Shiny", align = "center"),
+              h3("(Tutorial)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;")
       ),
       tabItem(tabName = "proj2",
-              h2("Project 2")
+              h2("How to Implement a File Upload Widget with a Preview Feature.", align = "center"),
+              h3("(Tutorial)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;")
       ),
       tabItem(tabName = "proj3",
-              h2("Project 3")
+              h2("Visualization", align = "center"),
+              h3("(Shiny App)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;")
       ),
       tabItem(tabName = "proj4",
-              h2("Project 4")
+              h2("Data Analysis Report", align = "center"),
+              h3("(Data Analysis Report)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;"),
+              h4("Data analysis report: 
+              Explore and analyze a dataset of interest to you to derive useful or 
+interesting insights. You must demonstrate the skills you are learning in the course (data 
+wrangling, data visualization, modeling, web scraping, text mining, etc.) and present one or more 
+key insights that can be learned from the data. Your project should demonstrate good habits with 
+respect to reproducibility, clear coding style and logic, and effective visualization and 
+communication.
+You may choose (or construct) any dataset of interest to you. If you are involved in research, 
+please feel free to use data from these projects. Otherwise, there are many datasets available 
+online you can work with, or you can build a dataset from the web yourself. We will explore 
+portfolio piece ideas and potential data sources throughout the semester. ")
       )
     )
   ))
--- Projects 1-4/app.R
+++ Projects 1-4/app.R
@@ -15,10 +15,10 @@ ui <- dashboardPage(
   dashboardHeader(title = "Projects"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("1. Test", tabName = "proj1",  icon = icon("image")),
-      menuItem("2. Test", tabName = "proj2", icon = icon("map")),
-      menuItem("3. Test", tabName = "proj3", icon = icon("file-code")),
-      menuItem("4. Test", tabName = "proj4", icon = icon("chart-bar"))
+      menuItem("1. Shiny App", tabName = "proj1",  icon = icon("file-code")),
+      menuItem("2. File Upload", tabName = "proj2", icon = icon("file-upload")),
+      menuItem("3. Visualization", tabName = "proj3", icon = icon("image")),
+      menuItem("4. Data Analysis Report", tabName = "proj4", icon = icon("chart-bar"))
     )
   ),
   
@@ -42,23 +42,44 @@ ui <- dashboardPage(
     #header code credit: https://stackoverflow.com/questions/45176030/add-text-on-right-of-shinydashboard-header
     #header code credit: https://stackoverflow.com/questions/45176030/add-text-on-right-of-shinydashboard-header
     tabItems(
-      tabItem(tabName = "Import",
-              h2("Import CSV File"),
-              fileInput("file", "Choose CSV File",multiple = FALSE, accept = NULL,
-                        width = NULL, buttonLabel = "Browse...",
-                        placeholder = "No file selected"),
-              actionButton("preview", "Preview Current Data Set"),
-              fluidRow(
-                mainPanel(
-                  dataTableOutput("impOut")
-                )
-              )
+      tabItem(tabName = "proj1",
+              h2("How to Compile your Work into One Simple App Using Shiny", align = "center"),
+              h3("(Tutorial)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;")
       ),
-      tabItem(tabName = "Convert",
-              h2("Convert CSV File")
+      tabItem(tabName = "proj2",
+              h2("How to Implement a File Upload Widget with a Preview Feature.", align = "center"),
+              h3("(Tutorial)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;")
       ),
-      tabItem(tabName = "Export",
-              h2("Export CSV File")
+      tabItem(tabName = "proj3",
+              h2("Visualization", align = "center"),
+              h3("(Shiny App)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;")
+      ),
+      tabItem(tabName = "proj4",
+              h2("Data Analysis Report", align = "center"),
+              h3("(Data Analysis Report)", align = "center"),
+              tags$hr(style=
+                        "border-color: black;
+                         border-width: 2px;"),
+              h4("Data analysis report: 
+              Explore and analyze a dataset of interest to you to derive useful or 
+interesting insights. You must demonstrate the skills you are learning in the course (data 
+wrangling, data visualization, modeling, web scraping, text mining, etc.) and present one or more 
+key insights that can be learned from the data. Your project should demonstrate good habits with 
+respect to reproducibility, clear coding style and logic, and effective visualization and 
+communication.
+You may choose (or construct) any dataset of interest to you. If you are involved in research, 
+please feel free to use data from these projects. Otherwise, there are many datasets available 
+online you can work with, or you can build a dataset from the web yourself. We will explore 
+portfolio piece ideas and potential data sources throughout the semester. ")
       )
     )
   ))
